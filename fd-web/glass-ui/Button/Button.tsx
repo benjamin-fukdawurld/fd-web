656f8a598b7b5be861c1e221bbcd6ca02f4bee28
benjamin <benjamin.zamour@gmail.com>
2021-12-15 20:17:52 -0400

    [glass-ui] Add Button

--- fd-web/glass-ui/Button/Button.tsx
+++ fd-web/glass-ui/Button/Button.tsx
@@ -1,33 +1,44 @@
-import React, { MouseEvent, useEffect, useRef } from "react";
+import React, { forwardRef, MouseEvent, useEffect, useRef } from "react";
 import { ButtonProps } from "./interfaces";
 import { ButtonContainer } from "./style";
 
-export default function Button({ forwardedAs, forwardedProps, ...props }: ButtonProps) {
-  const ref = useRef<HTMLElement>(null);
-  useEffect(() => {
-    if (!ref.current) {
-      return;
-    }
+import { useCombinedRefs } from "../../glass-ui/hooks";
 
-    ref.current.addEventListener("animationend", () => ref.current!.classList.remove("active"));
-  }, [1]);
+const Button = forwardRef<HTMLElement | null, ButtonProps>(
+  ({ forwardedAs, forwardedProps, ...props }: ButtonProps, ref): JSX.Element => {
+    const innerRef = useRef<HTMLElement | null>(null);
+    const combinedRefs = useCombinedRefs<HTMLElement | null>(ref, innerRef);
+    useEffect(() => {
+      if (!combinedRefs.current) {
+        return;
+      }
 
-  return (
-    <ButtonContainer
-      forwardedAs={forwardedAs ?? "button"}
-      forwardedProps={{
-        ref,
-        ...forwardedProps,
-      }}
-      {...props}
-      onClick={(event: MouseEvent<HTMLButtonElement>) => {
-        const elm = event.target as HTMLButtonElement;
-        elm.classList.add("active");
-      }}
-    >
-      <div className="button">
-        <div className="childrenContainer">{props.children}</div>
-      </div>
-    </ButtonContainer>
-  );
-}
+      combinedRefs.current.addEventListener("animationend", () =>
+        combinedRefs.current!.classList.remove("active")
+      );
+    }, [combinedRefs]);
+
+    return (
+      <ButtonContainer
+        ref={combinedRefs}
+        forwardedAs={forwardedAs ?? "button"}
+        forwardedProps={{
+          ...forwardedProps,
+        }}
+        {...props}
+        onClick={(event: MouseEvent<HTMLButtonElement>) => {
+          const elm = event.target as HTMLButtonElement;
+          elm.classList.add("active");
+        }}
+      >
+        <div className="button">
+          <div className="childrenContainer">{props.children}</div>
+        </div>
+      </ButtonContainer>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
