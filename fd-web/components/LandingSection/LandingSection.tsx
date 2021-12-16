b81ef944aadcab4b30e7b577ec9bc756cca8e4c8
benjamin <benjamin.zamour@gmail.com>
2021-12-15 20:23:01 -0400

    [fd-web] Add Landing Section

--- fd-web/components/LandingSection/LandingSection.tsx
+++ fd-web/components/LandingSection/LandingSection.tsx
@@ -1,19 +1,23 @@
 import styled, { useTheme } from "styled-components";
 
 import BaseSection, { SectionProps } from "../../glass-ui/Section";
+import { useVisible } from "../../glass-ui/hooks";
 
 import AboutButton from "../AboutButton";
 
 import IntroductionCard from "../IntroductionCard";
 
 import ContactButton from "../ContactButton";
+import { useEffect, useRef } from "react";
 
 const Section = styled(BaseSection)<SectionProps>`
   position: relative;
 
-  perspective: 1000rem;
+  perspective: 1000px;
   perspective-origin: center;
   transform-style: preserve-3d;
+
+  overflow: hidden;
 
   display: flex;
   justify-content: center;
@@ -24,10 +28,20 @@ const Section = styled(BaseSection)<SectionProps>`
 
 export default function LandingSection() {
   const theme = useTheme();
+
+  const [observe, visible] = useVisible({ threshold: 0.25, visible: true });
+  const landingSectionRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (landingSectionRef.current) {
+      observe(landingSectionRef.current);
+    }
+  }, [observe, landingSectionRef]);
+
   return (
     <Section>
       <AboutButton blurSize={"sm"} theme={theme} />
-      <IntroductionCard />
+      <IntroductionCard theme={theme} ref={landingSectionRef} visible={visible} />
       <ContactButton />
     </Section>
   );
