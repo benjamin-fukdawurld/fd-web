c7821d8b3af76bb0119534cad3a6eab5b69ef50b
benjamin <benjamin.zamour@gmail.com>
2021-12-15 20:19:46 -0400

    [glass-ui] Add Section

--- fd-web/glass-ui/Section.tsx
+++ fd-web/glass-ui/Section.tsx
@@ -1,10 +1,16 @@
+import { ForwardedRef, forwardRef } from "react";
 import Screen, { defaultProps, ScreenProps } from "./Screen";
 
-export default function Section({ min, ...props }: ScreenProps) {
-  return <Screen forwardedAs="section" min={min} {...props} />;
-}
+const Section = forwardRef<HTMLElement, ScreenProps>(
+  ({ min, ...props }: ScreenProps, ref: ForwardedRef<HTMLElement>) => {
+    return <Screen ref={ref} forwardedAs="section" min={min} {...props} />;
+  }
+);
 
+Section.displayName = "Section";
 Section.defaultProps = defaultProps;
 
 export { defaultProps };
 export type { ScreenProps as SectionProps };
+
+export default Section;
