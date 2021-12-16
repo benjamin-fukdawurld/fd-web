1a4b95f7ca2b8d5a085afcc96d01b122ce5fc18c
benjamin <benjamin.zamour@gmail.com>
2021-12-15 20:21:34 -0400

    [fd-web] Add utility classes

---
+++ fd-web/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Header(props: any) {
+  return <header style={{ display: "none" }}>header</header>;
+}
---
+++ fd-web/components/Layout/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const LayoutContainer = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+`;
+
+export { LayoutContainer };
