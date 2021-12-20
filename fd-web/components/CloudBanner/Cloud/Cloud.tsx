09d737de3463717cac7a0b5da46c4cc8883ec5b9
benjamin <benjamin.zamour@gmail.com>
2021-12-20 13:49:42 -0400

    [fd-web] Add CloudBanner

---
+++ fd-web/components/CloudBanner/Cloud/Cloud.tsx
@@ -0,0 +1,12 @@
+import { CloudContainer } from "./style";
+
+export default function Cloud(props: any) {
+  return (
+    <CloudContainer>
+      <div className="clouds">
+        <div className="image"></div>
+        <div className="image"></div>
+      </div>
+    </CloudContainer>
+  );
+}
