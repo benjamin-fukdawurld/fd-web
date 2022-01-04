54032331f5ed8a1eb0d3b0c7a29aefe83803f401
benjamin <benjamin.zamour@gmail.com>
2022-01-03 23:27:00 -0400

    [fd-web] Add about page

---
+++ fd-web/pages/about.tsx
@@ -0,0 +1,32 @@
+import Page from "../components/Page";
+import LandingSection from "../components/LandingSection";
+import ProjectSection from "../components/ProjectSection";
+import ProfileSection from "../components/ProfileSection";
+
+import SolarSystemComponent from "../components/SolarSystemComponent";
+import CloudBanner from "../components/CloudBanner";
+import { useRef, useState } from "react";
+
+import Loader from "../components/Loader";
+
+export default function Home(props: any) {
+  const [sceneLoaded, setSceneLoaded] = useState<boolean>(false);
+  const loaderRef = useRef<HTMLElement>(null);
+
+  return (
+    <Page
+      title="Benjamin Zamour - Ingénieur web full stack"
+      description="Indicateurs et gestion de contenu de FD"
+    >
+      <SolarSystemComponent
+        onLoaded={() => {
+          setTimeout(() => setSceneLoaded(true), 1000);
+          if (loaderRef.current) {
+            loaderRef.current.classList.add("fade");
+          }
+        }}
+      />
+      {!sceneLoaded ? <Loader ref={loaderRef} /> : <></>}
+    </Page>
+  );
+}
